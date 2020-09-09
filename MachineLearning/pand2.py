124fa6c408a1077601dee6a75ff5cb7d68153a56
unknown <pabloaugustodequadros@gmail.com>
2020-09-09 02:19:47 -0300

    aa

---
+++ MachineLearning/pand2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+def linha():
+    print(40*"-")
+
+
+alunos = {"Nome":["Paulo","Pablo","Aline","Laysa"],
+          "Nota":[4,7,5.5,9],
+          "Aprovado":["Não","Sim","Não","Sim"]}
+alunosDF = pd.DataFrame(alunos)
+dados = alunosDF
+print(alunosDF)
+linha()
+print(alunosDF.shape)
+linha()
+print(alunosDF.describe())
+linha()
+print(alunosDF["Nome"])
+linha()
+print(alunosDF.loc[[2]])
+linha()
+print(alunosDF.loc[alunosDF["Aprovado"]=="Sim"])
+linha()
+print(dados["Nome"].value_counts())
